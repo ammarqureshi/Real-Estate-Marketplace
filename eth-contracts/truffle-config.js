d6168ed820b78707920e575893d96516a084d287
ammar <user@Users-MacBook-Pro.local>
2020-08-12 00:31:29 +0100

    updated rinkeby deployment config

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -22,7 +22,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 const fs = require('fs');
-const mnemonic = 'more pink among attend smoke emotion slim finger copper clog cactus stage';
+const mnemonic = '';
 
 module.exports = {
   /**
